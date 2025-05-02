061b575a1d87c90e347953d5ab12d54176dfc160
STRUGAR <ana.strugar@stud.ubbcluj.ro>
2025-05-02 15:08:04 +0300

    incercarea__3_2 mai

--- train.py
+++ train.py
@@ -22,13 +22,61 @@ from utils.data_loading import BasicDataset, CarvanaDataset
 from utils.data_loading import EyeglassDataset
 from utils.dice_score import dice_loss
 
+import os
+import wandb
+import torch
+import logging
+import numpy as np
+
+
+class CheckpointSaver:
+    def __init__(self, dirpath, wandb_enabled, decreasing=True, top_n=5):
+        """
+        dirpath: Directory path where to store all model weights
+        decreasing: If decreasing is `True`, then lower metric is better
+        top_n: Total number of models_dir to track based on validation metric value
+        """
+        if not os.path.exists(dirpath): os.makedirs(dirpath)
+        self.dirpath = dirpath
+        self.wandb_enabled = wandb_enabled
+        self.top_n = top_n
+        self.decreasing = decreasing
+        self.top_model_paths = []
+        self.best_metric_val = np.Inf if decreasing else -np.Inf
+
+    def __call__(self, model, epoch, metric_val):
+        model_path = os.path.join(self.dirpath, model.__class__.__name__ + f'_epoch{epoch}.pt')
+        save = metric_val < self.best_metric_val if self.decreasing else metric_val > self.best_metric_val
+        if save:
+            logging.info(
+                f"Current metric value better than {metric_val} better than best {self.best_metric_val}, saving model at {model_path}, & logging model weights to W&B.")
+            self.best_metric_val = metric_val
+            torch.save(model.state_dict(), model_path)
+            if self.wandb_enabled:
+                self.log_artifact(f'model-ckpt-epoch-{epoch}.pt', model_path, metric_val)
+            self.top_model_paths.append({'path': model_path, 'score': metric_val})
+            self.top_model_paths = sorted(self.top_model_paths, key=lambda o: o['score'], reverse=not self.decreasing)
+        if len(self.top_model_paths) > self.top_n:
+            self.cleanup()
+
+    def log_artifact(self, filename, model_path, metric_val):
+        artifact = wandb.Artifact(filename, type='model', metadata={'Validation score': metric_val})
+        artifact.add_file(model_path)
+        wandb.run.log_artifact(artifact)
+
+    def cleanup(self):
+        to_remove = self.top_model_paths[self.top_n:]
+        logging.info(f"Removing extra models_dir.. {to_remove}")
+        for o in to_remove:
+            os.remove(o['path'])
+        self.top_model_paths = self.top_model_paths[:self.top_n]
 
 dir_img = Path('./data/imgs/')
 dir_mask = Path('./data/masks/')
 dir_checkpoint = Path('./checkpoints/')
 
 import wandb
-wandb.login()
+#wandb.login()
 wandb.login(key="1da3729d6dfaff15faa7afc015bfa5857dfc0b59")  # Correct method
 
 
