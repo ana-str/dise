a6de3de7180cfed622e462c78c4a421502e449c7
STRUGAR <ana.strugar@stud.ubbcluj.ro>
2025-05-02 15:47:47 +0300

    4 batches acum  facem asa

--- train.py
+++ train.py
@@ -115,7 +115,8 @@ def train_model(
 
     loader_args = dict(batch_size=batch_size, num_workers=os.cpu_count(), pin_memory=True)
 
-    base_dir = '/data/image_databases/detection_benchmark/gt_boxing'
+    #base_dir = '/data/image_databases/detection_benchmark/gt_boxing'
+    base_dir = 'C:/Users/strug/Desktop/imagini_si_masti_de_test'
 
     # Initialize lists for train and test directories
     train_dirs = []
@@ -335,7 +336,7 @@ def train_model(
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=8, help='Batch size')
+    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=4, help='Batch size')
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5, #modific in rularea 2
                         help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
